a071bbcc60fba805c02c22e00f24530134cf1983
MJS\wen_bingyan <663648512@qq.com>
2017-07-24 17:16:02 +0800

    阿达

--- sample.php
+++ sample.php
@@ -10,7 +10,7 @@ $signPackage = $jssdk->GetSignPackage();
   <title></title>
 </head>
 <body>
-  
+  5555555555555
 </body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
